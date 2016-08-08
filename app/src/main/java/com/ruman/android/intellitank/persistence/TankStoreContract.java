adaefe4f4c47f9dc3ae777399cbe99e34d3cded1
wruman <williamruman@live.com>
2016-08-08 07:39:07 -0500

    Adds sql for create / drop of tank table store

--- app/src/main/java/com/ruman/android/intellitank/persistence/TankStoreContract.java
+++ app/src/main/java/com/ruman/android/intellitank/persistence/TankStoreContract.java
@@ -1,11 +1,27 @@
 package com.ruman.android.intellitank.persistence;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
 /**
  * Created by will.ruman on 8/1/16.
  */
 public class TankStoreContract {
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String INT_TYPE = " INTEGER";
+    private static final String INT_PRIMARY_KEY = " INTEGER PRIMARY KEY";
+    private static final String COMMA_SEP = ",";
+    private static final String SQL_CREATE =
+            "CREATE TABLE " + TankTable.TABLE_NAME + " (" +
+            TankTable._ID + INT_PRIMARY_KEY + COMMA_SEP +
+            TankTable.COL_TANK_ID + INT_TYPE + COMMA_SEP +
+            TankTable.COL_TANK_NAME + TEXT_TYPE + COMMA_SEP +
+            TankTable.COL_TANK_TYPE + TEXT_TYPE + ")";
+    private static final String SQL_DROP =
+            "DROP TABLE IF EXISTS " + TankTable.TABLE_NAME;
 
     public TankStoreContract() {}
 
@@ -14,5 +30,26 @@ public class TankStoreContract {
         public static final String COL_TANK_ID = "id";
         public static final String COL_TANK_NAME = "name";
         public static final String COL_TANK_TYPE = "type";
+    }
+
+    public class TankStoreDbHelper extends SQLiteOpenHelper {
+
+        private final static int DATABASE_VERSION = 1;
+        private final static String DATABASE_NAME = "TankStore.db";
+
+        public TankStoreDbHelper(Context ctx) {
+            super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+            db.execSQL(SQL_DROP);
+            onCreate(db);
+        }
     }
 }
